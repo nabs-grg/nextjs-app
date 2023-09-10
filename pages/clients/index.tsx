a6b2d202dbea6a5beb0bce1f5063426c31eb5b15
nabs-grg <narbu.grg@gmail.com>
2023-09-10 23:07:07 +0100

    get static props

--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -1,5 +1,27 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default function Client() {
-  return <div>client page</div>;
+  const clients = [
+    {
+      id: 'george',
+      name: 'George',
+    },
+    {
+      id: 'adam',
+      name: 'Adam',
+    },
+  ];
+  return (
+    <div>
+      client page
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,64 @@ import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 import Link from 'next/link';
+import path from 'path';
+import fs from 'fs/promises';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function HomePage() {
+export default function HomePage(props: any) {
+  const { cars } = props;
+  console.log(cars[0].name, 'cars');
   return (
     <div className={styles.container}>
       Hello WORLD
       <ul>
-        <li>
+        {/* <li>
           <Link href="/portfolio">Portfolio</Link>
-        </li>
-        <li>
+        </li> */}
+        {/* <li>
           <Link href="/clients">Portfolio</Link>
-        </li>
+        </li> */}
       </ul>
+      {cars.map((car: any) => (
+        <ul key={car.id}>
+          <li>{car.name}</li>
+        </ul>
+      ))}
     </div>
   );
+}
+
+export async function getStaticProps() {
+  console.log('regenrating....');
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData as any);
+
+  return {
+    props: {
+      cars: [
+        {
+          logo: 'https://www.car-logos.org/wp-content/uploads/2011/09/bmw.png',
+          name: 'BMW',
+          id: 1,
+        },
+        {
+          logo: 'https://www.car-logos.org/wp-content/uploads/2011/09/marchedrs.png',
+          name: 'Mercedes',
+          id: 2,
+        },
+        {
+          logo: 'https://www.car-logos.org/wp-content/uploads/2011/09/mclaren.png',
+          name: 'Mclaren',
+          id: 3,
+        },
+        {
+          logo: 'https://www.car-logos.org/wp-content/uploads/2011/09/audi.png',
+          name: 'Audi',
+          id: 4,
+        },
+      ],
+    },
+  };
 }
